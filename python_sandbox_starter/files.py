f7983635933525897038fdad896eb61181ce89ca
Jake Scudder <scudsterr@gmail.com>
2020-11-10 11:55:10 -0500

    Practice finished

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,35 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+myFile = open("myfile.txt", "w")
+
+# Get info
+# print("Name: ", myFile.name)
+# print("Is Closed: ", myFile.closed)
+# print("Opening Mode: ", myFile.mode)
+
+# Write to file
+myFile.write("I love Python-- SIKE")
+myFile.write(" and JavaScript")
+myFile.close()
+
+# Append to file
+
+myFile = open("myfile.txt", "a")
+myFile.write(" . YAYAYAYAYA")
+myFile.close()
+
+# Read from file
+# myFile = open("myfile.txt", "r+")
+# text = myFile.read(111)
+# print(text)
+
+# Re-Write
+myFile = open("myfile.txt", "w")
+myFile.write("Begin again, FRESH")
+myFile.close()
+
+# and then read
+myFile = open("myfile.txt", "r+")
+text = myFile.read(111)
+print(text)
--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,11 +1,24 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
-
+# Core modules
 import datetime
 import time
 from time import time
 
-today = datetime.date.today()
+# Pip module
+from camelcase import CamelCase
 
-timestamp = time()
+# Import custom module
+from validator import validate_email
 
-print(timestamp)
+# today = datetime.date.today()
+# timestamp = time()
+
+# c = CamelCase()
+# print(c.hump("hello there world"))
+
+email = "test#test.com"
+
+if validate_email(email):
+    print("Email is valid")
+else:
+    print("Email is bad")
